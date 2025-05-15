1e9bb130995a89c7ab9a29d0ecbe87538c47f0ea
yehiad <yehiad82@gmail.com>
2025-05-15 09:57:43 +0300

    ≡ƒöÑ ╫ó╫ô╫¢╫ò╫ƒ ╫¢╫ò╫£╫£ ╫£╫ñ╫Ö .gitignore

--- posts/mailer.js
+++ posts/mailer.js
@@ -1,4 +1,7 @@
 const nodemailer = require("nodemailer");
+const fs = require("fs");
+const path = require("path");
+const os = require("os");
 const config = require("./email-config");
 
 const transporter = nodemailer.createTransport({
@@ -20,10 +23,17 @@ const sendMail = (subject, text, html) => {
 };
 
 const sendErrorMail = (subject, text) => {
+  let hostname;
+  try {
+    hostname = fs.readFileSync(path.join(__dirname, "instance-name.txt"), "utf-8").trim();
+  } catch {
+    hostname = os.hostname(); // fallback
+  }
+
   return transporter.sendMail({
     from: `"postify Error" <${config.user}>`,
     to: config.errorTo,
-    subject,
+    subject: `❌ שגיאה משרת ${hostname} | ${subject}`,
     text,
   });
 };

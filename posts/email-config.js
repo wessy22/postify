7e7479539d3e03e730372b5849c3490fb4f3da86
yehiad <yehiad82@gmail.com>
2025-10-21 09:44:39 +0300

    ≡ƒöÑ ╫ó╫ô╫¢╫ò╫ƒ ╫¢╫ò╫£╫£ ╫⌐╫£ ╫¢╫£ ╫ö╫º╫æ╫ª╫Ö╫¥

--- posts/email-config.js
+++ posts/email-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   user: "support@postify.co.il",         // כתובת המייל השולחת
   pass: "mwib fxwi ncwc vwzd",         // סיסמת אפליקציה (לא רגילה!)
-  to: "yehiad82@gmail.com",               // כתובת היעד שתקבל התראות
+  to: "postify.user@gmail.com",               // כתובת היעד שתקבל התראות
   errorTo: "postify.user@gmail.com"        // מייל לקבלת שגיאות מערכת
 };
